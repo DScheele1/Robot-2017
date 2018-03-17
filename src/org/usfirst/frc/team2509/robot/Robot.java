a255df65dd3ed81e53191656e51bdfc25ac5a1f1
Tigerbots2509-Member <2509Tigerbots@gmail.com>
2018-03-17 18:18:50 -0500

    update

--- src/org/usfirst/frc/team2509/robot/Robot.java
+++ src/org/usfirst/frc/team2509/robot/Robot.java
@@ -7,25 +7,28 @@
 
 package org.usfirst.frc.team2509.robot;
 
-import org.usfirst.frc.team2509.robot.commands.OperatorDrive;
-import org.usfirst.frc.team2509.robot.subsystems.DriveTrain;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
+ * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the build.properties file in the
  * project.
  */
-public class Robot extends TimedRobot {
-	
-	public static OI oi;
-	public static DriveTrain drivetrain;
-	public Command operatorDrive;
+public class Robot extends IterativeRobot {
+	public Joystick OPSTICK;
+	public Joystick COOPSTICK;
+	public static WPI_TalonSRX Leftfront;
+	public static WPI_TalonSRX Rightfront;
+	public static WPI_TalonSRX Leftback;
+	public static WPI_TalonSRX Rightback;
+	public static WPI_TalonSRX Sweeper;
+	public static MecanumDrive RobotDrive;
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -33,25 +36,14 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void robotInit() {
-		RobotMap.init();
-		operatorDrive = new OperatorDrive();
-		drivetrain = new DriveTrain();
-		oi = new OI();
-	}
-
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	@Override
-	public void disabledInit() {
-
-	}
-
-	@Override
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
+		OPSTICK= new Joystick(0);
+		COOPSTICK=  new Joystick(1);
+		Rightback = new WPI_TalonSRX(3);
+		Rightfront = new WPI_TalonSRX(1);
+		Leftback = new WPI_TalonSRX(0);
+		Leftfront = new WPI_TalonSRX(2);
+		Sweeper = new WPI_TalonSRX(5);
+		RobotDrive = new MecanumDrive(Leftfront, Leftback, Rightfront, Rightback);
 	}
 
 	/**
@@ -59,11 +51,11 @@ public class Robot extends TimedRobot {
 	 * between different autonomous modes using the dashboard. The sendable
 	 * chooser code works with the Java SmartDashboard. If you prefer the
 	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
+	 * getString line to get the auto name from the text box below the Gyro
 	 *
-	 * <p>You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
 	@Override
 	public void autonomousInit() {
@@ -74,12 +66,7 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	@Override
-	public void teleopInit() {
-		operatorDrive.start();
+		
 	}
 
 	/**
@@ -87,7 +74,16 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
+		Leftback.setInverted(true);
+		Leftfront.setInverted(true);
+		Rightback.setInverted(true);
+		Rightfront.setInverted(true);
+		RobotDrive.driveCartesian(OPSTICK.getX()*1,OPSTICK.getY()*1,OPSTICK.getZ()*1);
+		if(COOPSTICK.getRawButton(1)) {
+			Sweeper.set(0.5);
+		}else {
+			Sweeper.set(0);
+		}
 	}
 
 	/**
@@ -96,4 +92,7 @@ public class Robot extends TimedRobot {
 	@Override
 	public void testPeriodic() {
 	}
+	public double getScaledDrive(double input) {
+		return (input*((OPSTICK.getRawAxis(3)+3)*0.25));
+    }
 }
